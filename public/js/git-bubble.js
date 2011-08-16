228760543e7ddf3531c4a09a76cb9eb7f9d85989
Vitor Baptista <vitor@vitorbaptista.com>
2011-08-16 18:11:41 -0300

    Changes expected data structure

--- public/js/git-bubble.js
+++ public/js/git-bubble.js
@@ -10,6 +10,16 @@ function str_to_color(str) {
   return "#" + hex.slice(-6);
 }
 
+function format_date(date) {
+  return date.getDate() + "/" + (date.getMonth() + 1);
+}
+
+function is_other_day(date1, date2) {
+  var day = 24 * 60 * 60 * 1000;
+
+  return (date2 - date1) > day;
+}
+
 Raphael.fn.commit = function (x, y, radius, author) {
   var color = str_to_color(author),
       radius_growth = radius * 1.5,
@@ -36,7 +46,7 @@ Raphael.fn.commit = function (x, y, radius, author) {
   }
 };
 
-Raphael.fn.xy_axis = function (x, y, width, height, values) {
+Raphael.fn.xy_axis = function (x, y, width, height, commits) {
   var axis_color = "#bbd1d9",
       grid_color = "#d8e4eb",
       thickness = 10,
@@ -62,33 +72,55 @@ Raphael.fn.xy_axis = function (x, y, width, height, values) {
   }
 
   // Fills X Axis
-  var num_x_values = Object.keys(values).length;
-  var x_offset = (width - thickness) / (num_x_values + 1),
-      actual_x = x + thickness + x_offset;
+  var num_commits = Object.keys(commits).length,
+      get_commit_x = function (date) {
+        var hashes = Object.keys(commits),
+            first_commit = commits[hashes[0]],
+            last_commit = commits[hashes[hashes.length - 1]],
+            first_date = new Date(first_commit.date).getTime(),
+            last_date = new Date(last_commit.date).getTime();
 
-  for (key in values) {
-    this.rect(actual_x, y, 0.5, height + thickness * 2).attr({fill: grid_color, stroke: "none"});
-    this.text(actual_x, y + height + thickness * 3, key);
-    for (i in values[key]) {
-      var commit = values[key][i],
-          commit_y = y_min + (height - y_offset) * (-commit.ratio + 1);
+        return (function (date) {
+          var time = date.getTime(),
+              offset = 50;
 
-      this.commit(actual_x, commit_y, commit.size, commit.author);
+          return (time - first_date) * ((width - offset * 2) / (last_date - first_date)) + (x + offset);
+        });
+      }();
+
+  var last_date = null;
+  for (hash in commits) {
+    var commit = commits[hash],
+        commit_y = y_min + (height - y_offset) * (-commit.ratio + 1),
+        date = new Date(commit.date);
+
+    this.commit(get_commit_x(date), commit_y, commit.size, commit.author);
+
+    if (is_other_day(last_date, date)) {
+      date.setHours(12);
+      date.setMinutes(0);
+      var commit_x = get_commit_x(date)
+      this.rect(commit_x, y, 0.5, height + thickness * 2).attr({fill: grid_color, stroke: "none"});
+      this.text(commit_x, y + height + thickness * 3, format_date(date));
+
+      last_date = date;
     }
-    actual_x += x_offset
   }
-
 };
 
 window.onload = function () {
   var paper = Raphael("canvas", 800, 600);
-  var values = {
-    "01/08": [{ratio: 0.6, size: 20, author: "vitorbaptista"}, {ratio: 0, size: 10, author: "bozo"}],
-    "02/08": [{ratio: 1, size: 10, author: "God"}, {ratio: 0.3, size: 45, author: "monkey"}],
-    "03/08": [{ratio: 0.5, size: 30, author: "God"}, {ratio: 0.2, size: 32, author: "monkey"}],
-    "04/08": [{ratio: 0.4, size: 50, author: "God"}, {ratio: 0.8, size: 42, author: "monkey"}],
+  var commits = {
+    "hash-1": {date: "August 8, 2011 00:00:00", ratio: 0.6, size: 20, author: "vitorbaptista"},
+    "hash-2": {date: "August 8, 2011 11:33:00", ratio: 0, size: 10, author: "bozo"},
+    "hash-3": {date: "August 8, 2011 21:02:00", ratio: 1, size: 10, author: "God"},
+    "hash-4": {date: "August 9, 2011 14:54:00", ratio: 0.3, size: 45, author: "monkey"},
+    "hash-5": {date: "August 9, 2011 19:00:00", ratio: 0.5, size: 30, author: "God"},
+    "hash-6": {date: "August 10, 2011 03:00:00", ratio: 0.2, size: 32, author: "monkey"},
+    "hash-7": {date: "August 10, 2011 07:30:00", ratio: 0.4, size: 50, author: "God"},
+    "hash-8": {date: "August 10, 2011 23:53:00", ratio: 0.8, size: 42, author: "monkey"},
   };
 
   paper.rect(0, 0, paper.width, paper.height, 10).attr({fill: "#fff", stroke: "none"});
-  paper.xy_axis(10, 10, 800, 600, values);
+  paper.xy_axis(10, 10, 800, 600, commits);
 };
